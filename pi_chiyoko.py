19b37434628d1f5649780e9daea95048d3dc7b02
Prafulla Giri <pratheblackdiamond@gmail.com>
2017-04-05 14:01:05 +0545

    pi_chiyoko.py is just a copy of chiyoko.py

---
+++ pi_chiyoko.py
@@ -0,0 +1,217 @@
+#!/usr/bin/python3
+
+from collections import OrderedDict
+from time import time
+import argparse, os, re, subprocess, sys
+
+ImageProcessor = "convert -verbose -resize %s '%s' '%s'"
+VideoProcessor = "avconv -loglevel quiet -y -i '%s' -b:v 698k -b:a 94k -ar 48000 -s 640x512 '%s'"
+
+def figureExportPath(filePath, SOURCE, DEST):
+	"""Figures out the export path for a given file"""
+	filePath = os.path.abspath(filePath)
+	SOURCE = os.path.abspath(SOURCE)
+	DEST = os.path.abspath(DEST)
+	refDir = os.path.basename(SOURCE)
+	filePath_List = filePath.split(os.sep)
+	reqTail_List = filePath_List[filePath_List.index(refDir):]
+	reqTail = '%s' % os.sep .join(reqTail_List)
+	reqPath = os.path.join(DEST, reqTail)
+	return reqPath
+
+def createReqExportPath(reqPath):
+	"""Checks whether or not the required path exists; creates non-existant"""
+	reqPath = os.path.abspath(reqPath)
+	if not os.path.isdir(os.path.basename(reqPath)):
+		reqPath = os.path.split(reqPath)[0]
+	if not os.path.exists(reqPath):
+		os.makedirs(reqPath)
+
+def isImage(givenFile):
+	"""Returns a Boolean value. True or False. Rough definition of an Image"""
+	subject = os.path.abspath(givenFile)
+	isImg = 'image' in subprocess.getoutput("file --mime-type '%s'" % subject)
+	return isImg
+
+def isVideo(givenFile):
+	"""Checks Whether a File is a Video or not -> Boolean"""
+	subject = os.path.abspath(givenFile)
+	isVid = 'video' in subprocess.getoutput("file --mime-type '%s'" % subject)
+	return isVid
+
+def main():
+
+	initTime = time()
+	parser = argparse.ArgumentParser(description="clone a directory hierarchy "
+ 		+ "and compress multimedia files along the way")
+	parser.add_argument('SOURCE', help='Input Path')
+	parser.add_argument('DESTINATION', help='Output Path' + 
+		" set to '__in-place__' to make the modifications in place")
+	parser.add_argument('-I', '--Image-resize-scale', metavar='n',
+			help='Turns on image resizing [default resize scale: 1000]',
+			type=int, nargs='?', const=1000) 
+	parser.add_argument('-V', '--Video',
+ 			help='Turns on video compression',
+			action='store_true')
+	args = parser.parse_args()
+
+	SOURCE = os.path.abspath(args.SOURCE)
+	UNABRIDGED_SOURCE = SOURCE
+	if not args.DESTINATION == '__in-place__':
+		DEST = os.path.abspath(args.DESTINATION)
+	else:
+		DEST = os.path.dirname(SOURCE)
+	ResizeScale = args.Image_resize_scale
+
+	exportPath = figureExportPath(SOURCE, SOURCE, DEST)
+	if os.path.exists(exportPath) and not args.DESTINATION == '__in-place__':
+		print("""
+			   It seems that there already is a file named '%s' in the specified
+			   destination. Maybe it's the output of previous execution of this
+			   script. If you want to have a clone generated in the same destination
+			   please either rename or delete the file, and re-run this script.
+			  
+			  """ % os.path.basename(SOURCE))
+		sys.exit(1)
+
+	renamed_dict_src, SOURCE = singleQuoteHandler(SOURCE)
+	renamed_dict_dest, DEST = singleQuoteHandler(DEST, handleChildFiles=False)
+
+	ORIGINAL_SIZE = subprocess.getoutput("du -h '%s' | tail -n 1 | cut -f 1" % SOURCE)
+
+	for dirpath, dirnames, files in os.walk(SOURCE):
+		os.chdir(dirpath)
+		print()
+		print('*' * 10 + "Current Directory: %s" % dirpath + '*' * 10)
+		print()
+		if not (bool(dirnames) and bool(files)):
+			# Clone even the empty directories
+			dirpath = os.path.abspath(dirpath)
+			dir_exportPath = figureExportPath(dirpath, SOURCE, DEST)
+			os.makedirs(dir_exportPath, exist_ok=True)
+		for _file in files:
+			filePath = os.path.abspath(_file)	
+			exportPath = figureExportPath(filePath, SOURCE, DEST)
+			createReqExportPath(exportPath)
+			if bool(ResizeScale) and isImage(filePath):
+				print(subprocess.getoutput(ImageProcessor
+					 % (ResizeScale, _file, exportPath)))
+			elif bool(args.Video) and isVideo(filePath):
+				print("\nWorking on the video '%s'" % _file)
+				print("This will take quite a bit of time... please be patient")
+				start_time = time()
+				subprocess.getoutput(VideoProcessor % (_file, exportPath))
+				print("Processed '%s' in %f seconds"
+					 % (_file, (time()-start_time)), end='\n\n')
+			else:
+				print(subprocess.getoutput("cp -v '%s' '%s'" % 
+					(filePath, exportPath)))
+
+	currentExportPath = figureExportPath(SOURCE, SOURCE, DEST)
+	PROCESSED_SIZE = subprocess.getoutput("du -h '%s' | tail -n 1 | cut -f 1"
+		% currentExportPath)
+
+	clonedCorrector(renamed_dict_src, SOURCE, UNABRIDGED_SOURCE, DEST)
+	# because the cloned files should be corrected first, else, paths will be 'broken'
+	if not args.DESTINATION == '__in-place__' or exportPath == UNABRIDGED_SOURCE:
+		singleQuoteReverser(renamed_dict_dest)
+		singleQuoteReverser(renamed_dict_src)
+	
+	print("\nAll Done!")
+	print("Original:   %s\t%s" % (ORIGINAL_SIZE, UNABRIDGED_SOURCE)) 
+	print("Processed:  %s\t%s" % (PROCESSED_SIZE, exportPath)) 
+	print("\nTime Taken: %f seconds" % (time() - initTime))
+
+def singleQuoteHandler(PATH, handleChildFiles=True):	
+	"""
+	Goes through a directory - changes all the single quotes in the pathname PATH to
+	underscore characters. If handleChildFiles is set to False, will not change the
+	child dirs/files name.
+	"""
+	
+	INIT_WORKING_DIR = os.path.abspath(os.getcwd())
+
+	renamed = OrderedDict()
+	
+	fatalNameRegex = re.compile(r"'")
+	
+	while fatalNameRegex.search(PATH):
+		# Keep on renaming folder until none of the parent directories of the given
+		# folder have a single quote in their name
+		singleQuoteIndex = PATH.find("'")
+		succeedingSepIndex = PATH.find(os.sep, singleQuoteIndex)
+		if succeedingSepIndex == -1:
+			succeedingSepIndex = None
+		fatalPortion = PATH[:succeedingSepIndex]
+		os.chdir(os.path.dirname(fatalPortion))
+		properPortion = fatalNameRegex.sub('_', os.path.basename(fatalPortion))
+		properPath = os.path.dirname(fatalPortion) + os.sep + properPortion
+		os.rename(fatalPortion, properPath)
+		renamed[fatalPortion] = properPath
+		PATH = properPath + PATH[len(fatalPortion):]
+	
+	if handleChildFiles:
+		redoWalk = True
+		while redoWalk:
+			for dirPath, dirNames, files in os.walk(PATH):
+				redoWalk = False
+				os.chdir(dirPath)
+				if fatalNameRegex.search(dirPath):
+					properName = fatalNameRegex.sub('_', os.path.basename(dirPath))
+					dirPath = os.path.abspath(dirPath)
+					properPath = os.sep .join([os.path.dirname(dirPath), properName])
+					os.rename(dirPath, properPath)
+					renamed[dirPath] = properPath
+					redoWalk = True
+				for _dir in dirNames:
+					if fatalNameRegex.search(_dir):
+						properName = fatalNameRegex.sub('_', _dir)
+						_dirPath = os.path.abspath(_dir)
+						properPath = os.sep .join([os.path.dirname(_dirPath), properName])
+						os.rename(_dirPath, properPath)
+						renamed[_dirPath] = properPath
+						redoWalk = True
+				for _file in files:
+					if fatalNameRegex.search(_file):
+						filePath = os.path.abspath(_file)
+						properName = fatalNameRegex.sub('_', _file)
+						properPath = os.sep .join([os.path.dirname(filePath), properName])
+						os.rename(filePath, properPath)
+						renamed[filePath] = properPath
+						redoWalk = True
+				if redoWalk:
+					break
+
+	os.chdir(INIT_WORKING_DIR)
+
+	return renamed, PATH
+
+def singleQuoteReverser(renamed_dict):
+	"""Reverses The Changes Made By the singleQuoteHandler function.
+	   In the SOURCE!!! (only)
+	"""
+	# The reason why OrderedDict was employed was so that this traversal could be
+	# made from the child directories to parent directories so that none of the
+	# paths might be 'broken'. An ordered bottom-Up, instead of a disordered top-down.
+	for old_name in reversed(renamed_dict):
+		os.rename(renamed_dict[old_name], old_name)
+
+def clonedCorrector(renamed_dict, src, unabrSrc, dest):
+	"""Reverses the changes made by the singleQuoteHandler function in the processed directory
+	   hierarchy. So, this essentially figures out if a file in the cloned directory was rena-
+	   med and, if yes, changes the name back to the original.
+	   The renamed_dict to input to this function is the one returned by singleQuoteHandler on
+	   the SOURCE.
+	   unabrSrc -> The 'unabridged' source
+	"""
+	# Same here with the use of OrderedDict as with the previous function
+	for originalName in reversed(renamed_dict):
+		processedName = renamed_dict[originalName]
+		clonedName = figureExportPath(processedName, src, dest)
+		shouldBeNamed = figureExportPath(originalName, unabrSrc, dest)
+		if os.path.exists(clonedName):
+			os.rename(clonedName, shouldBeNamed)
+
+
+if __name__ == "__main__":
+	main()
